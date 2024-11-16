9cb516988f64aa09968a978273d41d11d4ddb607
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-16 17:54:10 +0300

    Assignment 3 solved

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Assignment 1
 const text = document.querySelector('.text');
 const buttonAppear = document.querySelector('#appear');
 const buttonDisappear = document.querySelector('#disappear');
@@ -14,3 +15,38 @@ function hideText() {
 
 buttonAppear.addEventListener('click', showText);
 buttonDisappear.addEventListener('click', hideText);
+
+
+// Assignment 3
+field.onclick = function (event) {
+
+    // координаты поля относительно окна браузера
+    let fieldCoords = this.getBoundingClientRect();
+
+    // мяч имеет абсолютное позиционирование (position:absolute), поле - относительное (position:relative)
+    // таким образом, координаты мяча рассчитываются относительно внутреннего, верхнего левого угла поля
+    let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+    };
+
+    // запрещаем пересекать верхнюю границу поля
+    if (ballCoords.top < 0) ballCoords.top = 0;
+
+    // запрещаем пересекать левую границу поля
+    if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+    // // запрещаем пересекать правую границу поля
+    if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+    }
+
+    // запрещаем пересекать нижнюю границу поля
+    if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.left = ballCoords.left + 'px';
+    ball.style.top = ballCoords.top + 'px';
+}

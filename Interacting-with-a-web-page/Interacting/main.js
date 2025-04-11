0011fa4c56d0a0ec9fd10544c17447030513097c
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-11 22:03:50 +0300

    Repositories are merged on the main branch

---
+++ Interacting-with-a-web-page/Interacting/main.js
@@ -0,0 +1,68 @@
+// Assignment 1
+const text = document.querySelector('.text');
+const buttonAppear = document.querySelector('#appear');
+const buttonDisappear = document.querySelector('#disappear');
+
+function showText() {
+    text.style.display = '';
+}
+
+function hideText() {
+    text.style.display = 'none';
+}
+
+buttonAppear.addEventListener('click', showText);
+buttonDisappear.addEventListener('click', hideText);
+
+
+// Assignment 3
+field.onclick = function (event) {
+
+    // координаты поля относительно окна браузера
+    let fieldCoords = this.getBoundingClientRect();
+
+    // мяч имеет абсолютное позиционирование (position:absolute), поле - относительное (position:relative)
+    // таким образом, координаты мяча рассчитываются относительно внутреннего, верхнего левого угла поля
+    let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+    };
+
+    // запрещаем пересекать верхнюю границу поля
+    if (ballCoords.top < 0) ballCoords.top = 0;
+
+    // запрещаем пересекать левую границу поля
+    if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+    // // запрещаем пересекать правую границу поля
+    if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+    }
+
+    // запрещаем пересекать нижнюю границу поля
+    if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+    }
+
+    ball.style.left = ballCoords.left + 'px';
+    ball.style.top = ballCoords.top + 'px';
+}
+
+
+// Assignment 4
+let menuElem = document.querySelector('#sweeties');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function () {
+    menuElem.classList.toggle('open');
+};
+
+
+// Assignment 5
+let panes = document.querySelectorAll('.pane');
+
+for (let pane of panes) {
+    pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">X</button>');
+    pane.firstChild.onclick = () => pane.remove();
+}

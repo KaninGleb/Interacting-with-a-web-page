8aef867ce9165f89b8f64673e8ce52c2a1ab2b57
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-04 00:22:21 +0300

    fin

--- Classes/01-Clock-with-Class/main.js
+++ Classes/01-Clock-with-Class/main.js
@@ -1,48 +1,63 @@
 class Clock {
   constructor({template}) {
-    this.template = template;
-    this.timer = null; // Initialize timer to null
+    this.template = template
+    this.timer = null
+    this.isStarted = false
   }
 
+  stop() {
+    clearInterval(this.timer)
+    this.timer = null
+    this.isStarted = false
+    document.querySelector('.clock').textContent = 'Press Start!'
+    document.querySelector('.clock').classList.add('initial-text')
+  }
+
+  start() {
+    if (!this.isStarted) {
+      this.isStarted = true
+      document.querySelector('.clock').classList.remove('initial-text')
+      this.tick()
+    }
+  }
+
+  tick() {
+    this.render();
+    let now = new Date()
+    let delay = 1000 - now.getMilliseconds()
+    this.timer = setTimeout(() => this.tick(), delay)
+  }
+  
   render() {
     let date = new Date();
 
     let hours = date.getHours();
     if (hours < 10) hours = '0' + hours;
 
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
 
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
+    let seconds = date.getSeconds();
+    if (seconds < 10) seconds = '0' + seconds;
 
     let output = this.template
       .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
+      .replace('m', minutes)
+      .replace('s', seconds);
 
     document.querySelector('.clock').textContent = output;
   }
-
-  stop() {
-    clearInterval(this.timer);
-    this.timer = null
-  }
-
-  start() {
-    if (!this.timer) {
-      this.render()
-      this.timer = setInterval(() => this.render(), 1000)
-    }
-  }
 }
 
-let clock = new Clock({template: 'h:m:s'});
+const clock = new Clock({ template: 'h:m:s' });
+
+document.querySelector('.clock').textContent = 'Press Start!';
+document.querySelector('.clock').classList.add('initial-text');
 
 document.getElementById('startBtn').addEventListener('click', () => {
   clock.start();
-});
+})
 
 document.getElementById('stopBtn').addEventListener('click', () => {
   clock.stop();
-});
+})
